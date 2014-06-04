3bb8dbafd9fd4756927f8518044375cf763ca930
Chris Mague <cmague@machinezone.com>
2014-06-03 17:00:18 -0700

    remove uncessary 1 and add more packages necessary to build

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,6 +1,7 @@
 ###################################
 #     Site.pp
 
+include epel
 include vagrant
 include unix_base
 include packagebuild
--- puppet/modules/packagebuild/manifests/init.pp
+++ puppet/modules/packagebuild/manifests/init.pp
@@ -15,7 +15,7 @@ class packagebuild {
     require  => Package['ruby-devel', 'rpm-build'],
   }
 
-  $build_utils = [ 'gcc', 'gcc-c++', 'autoconf', 'automake', 'libtool', 'cppunit-devel', 'cppunit', 'libicu', 'libicu-devel', 'python-setuptools', 'libxslt-devel' ]
+  $build_utils = [ 'gcc', 'gcc-c++', 'autoconf', 'automake', 'libtool', 'cppunit-devel', 'cppunit', 'libicu', 'libicu-devel', 'python-setuptools', 'libxslt-devel', 'pcre-devel', 'openssl-devel', 'gd-devel', 'GeoIP-devel' ]
 
   package { $build_utils:
     ensure => latest,
